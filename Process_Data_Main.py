4403a08fdac2a84bebb439327e59469e56841b10
khurram-a <khurram.a@esl.ac.uk>
2025-05-01 17:26:31 +0100

    Add files via upload

---
+++ Process_Data_Main.py
@@ -0,0 +1,65 @@
+#---------------------------------------------------
+#------Name : Process_Data_Modular_2.py
+#------Created  : 30/04/2025 - Khurram Asif
+#------Modified : 
+#------Description : Python file handling tutorial to
+#------------------: show modular code
+#----------------------------------------------------
+
+
+# import user defined calculation functions
+#try:
+import Calculation_Functions
+
+#except ModuleNotFoundError:
+       #print("Error : Make sure module file exists")
+
+
+# import the python built-in csv module
+import csv
+
+
+
+# initializing the students list
+students = []   
+
+
+try:
+        # reading csv file
+        with open('Students.csv', 'r') as file:
+                # creating a csv reader object
+                reader = csv.reader(file)
+
+                # Read the header (first row)
+                header = next(reader) 
+                
+                # extracting field names through first row
+                for row in reader:
+                        
+                        #data elements extracted from the row 
+                        name, math, science, english = row
+                        
+                        #calculate the average score
+                        average = Calculation_Functions.calculate_average([int(math), int(science), int(english)])
+                        
+                        #assign the grade
+                        grade = Calculation_Functions.assign_grade(average)
+                
+                        #append the data into the list
+                        students.append([name, math, science, english, average, grade])
+
+except FileNotFoundError:
+        # Handle case when the file is not found
+        print(f"Error: Students.csv not found.")
+
+#write the data into a new file
+with open('Student_Results.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Name', 'Math', 'Science', 'English', 'Average', 'Grade'])
+        writer.writerows(students)
+
+print('Results saved to student_results.csv')
+
+
+
+
